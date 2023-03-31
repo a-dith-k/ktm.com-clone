9a680de65c44520d25f69d9d9c12f5b53c865045
a-dith-k <adithk1308@gmail.com>
2023-03-31 10:56:31 +0530

    div close and open using javascript added

--- script.js
+++ script.js
@@ -21,4 +21,48 @@ function showDiv() {
         }
         
     }
+document.getElementById("grid-company").style.display="none";
+document.getElementById("grid-world").style.display="none";
+document.getElementById("grid-service").style.display="none";
+document.getElementById("grid-legal").style.display="none";
 
+    function showGrid1(){
+        let x=document.getElementById("grid-company")
+        if(x.style.display=="none"){
+            x.style.display="block"
+        }else{
+            x.style.display="none"
+        }
+    }
+
+function showGrid2(){
+    let y=document.getElementById("grid-world")
+    if(y.style.display=="none"){
+        y.style.display="block"
+    }else{
+        y.style.display="none"
+    }
+}
+
+function showGrid3(){
+    let x=document.getElementById("grid-service")
+    if(x.style.display=="none"){
+        x.style.display="block"
+    }else{
+        x.style.display="none"
+    }
+}
+
+function showGrid4(){
+    let x=document.getElementById("grid-legal")
+    if(x.style.display=="none"){
+        x.style.display="block"
+    }else{
+        x.style.display="none"
+    }
+}
+    
+
+
+
+    
